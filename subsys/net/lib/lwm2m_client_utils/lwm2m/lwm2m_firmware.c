71eb505f90b90dd9bf472ea84cafe9c3babae20e
Juha Heiskanen <juha.heiskanen@nordicsemi.no>
2022-12-21 12:46:36 +0200

    net: lib: FOTA update update
    Update process is calling Device reboot execute call.
    Now Application can handle reboot process safely.
    
    Signed-off-by: Juha Heiskanen <juha.heiskanen@nordicsemi.no>

--- subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_firmware.c
+++ subsys/net/lib/lwm2m_client_utils/lwm2m/lwm2m_firmware.c
@@ -34,6 +34,9 @@
 #include <modem/modem_info.h>
 #include <ncs_version.h>
 #include <pm_config.h>
+#ifndef CONFIG_ZTEST
+#include <zephyr/sys/reboot.h>
+#endif
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(lwm2m_firmware, CONFIG_LWM2M_CLIENT_UTILS_LOG_LEVEL);
@@ -224,16 +227,23 @@ static void target_image_type_store(uint16_t instance, int img_type)
 
 static void reboot_work_handler(void)
 {
-#if defined(CONFIG_LWM2M_CLIENT_UTILS_FIRMWARE_UPDATE_REBOOT) &&                                   \
-	defined(CONFIG_LWM2M_CLIENT_UTILS_DEVICE_OBJ_SUPPORT) && \
-	!defined(CONFIG_ZTEST)
+#if !defined(CONFIG_ZTEST)
+	/* Call reboot execute */
+	struct lwm2m_engine_res *dev_res;
+	struct lwm2m_obj_path path;
+	path.level = LWM2M_PATH_LEVEL_RESOURCE;
+	path.obj_id = LWM2M_OBJECT_DEVICE_ID;
+	path.obj_inst_id = 0;
+	path.res_id = 4;
+	dev_res = lwm2m_engine_get_res(&path);
 
-	lwm2m_device_reboot_cb(0, NULL, 0);
-#elif defined(CONFIG_LWM2M_CLIENT_UTILS_FIRMWARE_UPDATE_REBOOT) &&                                 \
-	defined(CONFIG_LWM2M_CLIENT_UTILS_MODEM_FW_UPDATE_AUTO_VALIDATE)
-	LOG_PANIC();
-	sys_reboot(SYS_REBOOT_COLD);
-	LOG_WRN("Rebooting");
+	if (dev_res && dev_res->execute_cb) {
+		dev_res->execute_cb(0, NULL, 0);
+	} else {
+		LOG_PANIC();
+		sys_reboot(SYS_REBOOT_COLD);
+		LOG_WRN("Rebooting");
+	}
 #endif
 }
 /************** Wrappers between normal FOTA object and Advanced FOTA object ********/
@@ -466,11 +476,8 @@ static void update_work_handler(struct k_work *work)
 
 	if (update_data.type == MODEM_DELTA) {
 		updated_instance = modem_obj_id;
-		if (IS_ENABLED(CONFIG_LWM2M_CLIENT_UTILS_FIRMWARE_UPDATE_REBOOT) ||
-		    IS_ENABLED(CONFIG_LWM2M_CLIENT_UTILS_ADV_FIRMWARE_UPDATE_OBJ_SUPPORT)) {
-			result = apply_firmware_delta_modem_update();
-			set_result(modem_obj_id, result);
-		}
+		result = apply_firmware_delta_modem_update();
+		set_result(modem_obj_id, result);
 	} else if (update_data.type == MODEM_FULL) {
 		updated_instance = modem_obj_id;
 #if defined(CONFIG_DFU_TARGET_FULL_MODEM)
